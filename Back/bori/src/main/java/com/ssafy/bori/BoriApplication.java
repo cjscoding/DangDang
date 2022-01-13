ea332f40c5e3f5a07e010afa0997828695b04fe6
AntCode97 <50140266+AntCode97@users.noreply.github.com>
2022-01-13 17:57:59 +0900

    chore: .idea삭제

---
+++ Back/bori/src/main/java/com/ssafy/bori/BoriApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoriApplication.class, args);
+    }
+
+}
