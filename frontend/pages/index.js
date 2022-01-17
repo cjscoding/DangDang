b63e0b505e28275a9502aaa914614bdb5b455642
김동유 <ehddb2252@gmail.com>
2022-01-18 00:19:22 +0900

    feat: 1인 면접연습 구현 준비

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <div>메인 페이지</div>
-  )
+  return <>
+    <Link href="/self-practice">
+      <a><h1>혼자연습한당</h1></a>
+    </Link>
+    <Link href="/">
+      <a><h1>스터디모인당</h1></a>
+    </Link>
+    <Link href="/">
+      <a><h1>스터디구한당</h1></a>
+    </Link>
+    <Link href="/">
+      <a><h1>당당알아본당</h1></a>
+    </Link>
+  </>
 };
