7ac78774a6caad3cbfe3c86045f41dc043e8a9e9
이윤준 <dbswns97@gmail.com>
2022-01-17 19:17:06 +0000

    chore : 옛날 Back 폴더 삭제

--- Back/dangdang/src/main/java/com/ssafy/dangdang/config/security/oauth/provider/OAuth2UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.dangdang.config.security.oauth.provider;
-
-public interface OAuth2UserInfo {
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getName();
-    String getImageUrl();
-}
