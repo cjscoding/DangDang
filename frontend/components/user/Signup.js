ffdb828e2718731091d8b1c6e5df4af4ccf99585
HyeIn-Kim <fallingstars2749@gmail.com>
2022-01-21 13:20:09 +0900

    [S06P12C203-21] feat: 로그인 모달창 & 컴포넌트 배치

---
+++ frontend/components/user/Signup.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+export default function Signup() {
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <Button text="Google로 시작하기" />
+      <Button text="Kakao로 시작하기" />
+      <Button text="이메일로 시작하기" />
+    </div>
+  );
+}
