f2c02b38f9b0f195cf40471c789d2f1ea79c5451
김혜인 <fallingstars2749@gmail.com>
2022-01-25 00:36:08 +0900

    [S06P12C203-164] feat: 질문게시판 테이블 컴포넌트 배치

---
+++ frontend/components/layout/table/tableColumn.js
@@ -0,0 +1,3 @@
+export default function tableColumn({ children }) {
+  return <td>{children}</td>;
+}
--- frontend/pages/interview-question/index.js
+++ frontend/pages/interview-question/index.js
@@ -1,3 +1,25 @@
+import Table from "../../components/layout/table/table";
+import TableRow from "../../components/layout/table/tableRow";
+import TableColumn from "../../components/layout/table/tableColumn";
+import styles from "../../scss/interview-question/main.module.scss";
+
 export default function interviewQuestion() {
-  return <div>인터뷰 질문</div>;
+  const headers = ["카테고리", "질문"];
+  return (
+    <section className={styles.container}>
+      <h1>인터뷰 질문</h1>
+      <Table headers={headers}>
+        <TableRow>
+          <TableColumn>인성면접</TableColumn>
+          <TableColumn>보리는 귀여운가요?</TableColumn>
+        </TableRow>
+        <TableRow>
+          <TableColumn>기술면접</TableColumn>
+          <TableColumn>
+            당당이의 MBTI를 예측하고 그 이유를 말해주세요.
+          </TableColumn>
+        </TableRow>
+      </Table>
+    </section>
+  );
 }
