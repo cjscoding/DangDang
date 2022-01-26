60516433cd6c6e3fbb100419eca370b7c1ceac91
김혜인 <fallingstars2749@gmail.com>
2022-01-26 11:19:46 +0900

    [S06P12C203-155] feat: 이메일 로그인 토큰 확인

--- frontend/api/user.js
+++ frontend/api/user.js
@@ -2,8 +2,8 @@ import { apiInstance } from "./index";
 
 const api = apiInstance();
 
-function getToken(loginParams, success, fail) {
-  api.get(`/user/login`, { params: loginParams }).then(success).catch(fail);
+function getToken(params, success, fail) {
+  api.post(`/user/login`, params).then(success).catch(fail);
 }
 
 function signUpRequest(params, success, fail) {
--- frontend/components/user/Login.js
+++ frontend/components/user/Login.js
@@ -1,15 +1,49 @@
 import Button from "./Button";
 import styles from "../../scss/user/login-signup.module.scss";
+import { getToken } from "../../api/user";
+import { useState } from "react";
 
 export default function Login({ onClick }) {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target: { id, value } }) => {
+    const nextValues = {
+      ...values,
+      [id]: value,
+    };
+    setValues(nextValues);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getToken(
+      values,
+      ({ headers: { authorization, refreshtoken } }) => {
+        console.log(authorization);
+        console.log(refreshtoken);
+      },
+      (error) => console.log(error)
+    );
+  };
+
   return (
     <div className={styles.body}>
       <h1>로그인</h1>
-      <form className={styles.form} method="post">
+      <form className={styles.form} method="post" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">
             이메일
-            <input id="email" type="email" placeholder="이메일" required />
+            <input
+              id="email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              placeholder="이메일"
+              required
+            />
           </label>
         </div>
         <div>
@@ -18,6 +52,8 @@ export default function Login({ onClick }) {
             <input
               id="password"
               type="password"
+              value={values.password}
+              onChange={handleChange}
               placeholder="비밀번호"
               required
             />
