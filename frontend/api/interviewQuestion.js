b7060fd7837b7357de5c27a4e963cfead2328930
김혜인 <fallingstars2749@gmail.com>
2022-02-06 00:34:31 +0900

    [S06P12C203-293] fix: Pageable 파라미터 적용

--- frontend/api/interviewQuestion.js
+++ frontend/api/interviewQuestion.js
@@ -2,8 +2,8 @@ import { apiInstance } from "./index";
 
 const api = apiInstance();
 
-export function getInterviewQuestions(success, fail) {
-  api.get(`/interview`).then(success).catch(fail);
+export function getInterviewQuestions(params, success, fail) {
+  api.get(`/interview`, params).then(success).catch(fail);
 }
 
 export function addInterviewQuestion(params, success, fail) {
