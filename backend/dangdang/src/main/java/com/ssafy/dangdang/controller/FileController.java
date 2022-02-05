5da1d0ff2f4c167d614dca0b273544f5a14a07e1
이윤준 <50140266+AntCode97@users.noreply.github.com>
2022-02-06 02:31:19 +0900

    fix : video corb 에러 해결

--- backend/dangdang/src/main/java/com/ssafy/dangdang/controller/FileController.java
+++ backend/dangdang/src/main/java/com/ssafy/dangdang/controller/FileController.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.net.MalformedURLException;
 
 @RestController
@@ -30,9 +32,11 @@ public class FileController {
         return new UrlResource("file:///" + imageLocation+filename);
     }
 
-    @GetMapping("/videos/{filename}")
+    @GetMapping(value = "/videos/{filename}", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @CrossOrigin(origins = {"http://localhost:3000"}, allowedHeaders = "*")
     public Resource showVideo(@PathVariable String filename) throws
             MalformedURLException {
+//        response.addHeader("X-Content-Type-Options", "nosniff");
         return new UrlResource("file:///" + videoLocation+filename);
     }
 }
