84ec5333e20dd5b412402c66485f5f5d5cde2c99
이윤준 <dbswns97@gmail.com>
2022-02-09 01:43:02 +0000

    Update index.js

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "https://i6c203.p.ssafy.io:8443";
 export const WEBRTC_URL = "https://i6c203.p.ssafy.io:8443";
-export const FRONTEND_URL = "http://i6c203.p.ssafy.io:3000"
+export const FRONTEND_URL = "http://localhost:3000"
