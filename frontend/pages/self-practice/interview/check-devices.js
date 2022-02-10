51dbc334f940878002b4d1250cfe85fa6642f3a7
조은지 <dmswl_0311@naver.com>
2022-02-10 13:54:50 +0900

    [S06P12C203-332] feat : 1인 면접 연습 준비화면(카메라,오디오 선택) UI (option 수정예정)

--- frontend/pages/self-practice/interview/check-devices.js
+++ frontend/pages/self-practice/interview/check-devices.js
@@ -50,17 +50,41 @@ function CheckDevices({preparedQuestions, ws, isQs, setQuestions}) {
   }, [])
 
   return <div className={styles.container}>
+    <div className={styles.pindicator}>
+      <div className={styles.bullet}>
+        <span className={styles.icon}>1</span>
+        <div className={styles.text}>Step 1</div>
+      </div>
+      <div className={styles.bullet}>
+        <span className={styles.icon}>2</span>
+        <div className={styles.text}>Step 2</div>
+      </div>
+      <div className={styles.bullet}>
+        <span className={styles.iconcur}><span>3</span></span>
+        <div className={styles.text}>Step 3</div>
+      </div>
+    </div>
+    <h1>카메라와 마이크 설정을 확인해주세요.</h1>
     <div className={styles.videoContainer}>
       <div><MyFace /></div>
       <div className={styles.selectContainer}>
-        <label htmlFor="camera-select">카메라</label><br/>
-        <CameraSelect id="camera-select" /><br/>
-        <label htmlFor="mic-select">마이크</label><br/>
-        <MicSelect id="mic-select" /><br/>
-        <label htmlFor="speaker-select">스피커</label><br/>
-        <SpeakerSelect id="speaker-select" /><br/>
+        <div>
+          <label htmlFor="camera-select"></label><br/>
+          <span className={styles.span}><i className="fas fa-camera"></i></span>
+          <CameraSelect id="camera-select"/><br/>
+        </div>
+        <div>
+          <label htmlFor="mic-select"></label><br/>
+          <span className={styles.span}><i className="fas fa-microphone"></i></span>
+          <MicSelect id="mic-select" /><br/>
+        </div>
+        <div>
+          <label htmlFor="speaker-select"></label><br/>
+          <span className={styles.span}><i className="fas fa-headphones"></i></span>
+          <SpeakerSelect id="speaker-select" /><br/>
+        </div>
       </div>
-      <button ref={nextBtn} className={styles.nextBtn}><h1>NEXT</h1></button>
+      <button ref={nextBtn} className={styles.nextBtn}>연습 시작하기!</button>
     </div>
   </div>
 }
