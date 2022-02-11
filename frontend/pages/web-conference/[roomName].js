c39f372cf75a4dc2941bfdd2f6c42fe9c1e6ba4d
김동유 <ehddb2252@gmail.com>
2022-02-11 22:01:51 +0900

    feat: 화면공유 한명만 가능하게 하는 로직 추가

--- frontend/pages/web-conference/[roomName].js
+++ frontend/pages/web-conference/[roomName].js
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { connect } from "react-redux";
 import CameraSelect from "../../components/webRTC/devices/CameraSelect";
 import MicSelect from "../../components/webRTC/devices/MicSelect";
+import SpeakerSelect from "../../components/webRTC/devices/SpeakerSelect";
 import getVideoConstraints from "../../components/webRTC/getVideoConstraints";
 import styles from "../../scss/web-conference/mainComponent.module.scss";
 import SockJS from "sockjs-client";
@@ -14,23 +15,22 @@ function mapStateToProps(state) {
     myIdName: `${state.userReducer.user.id}-${state.userReducer.user.nickName}`, // id + - + nickName
     cameraId: state.videoReducer.cameraId,
     micId: state.videoReducer.micId,
+    speakerId: state.videoReducer.speakerId,
   };
 }
 export default connect(mapStateToProps, null)(Conference);
-function Conference({ws, myIdName, cameraId, micId}) {
+function Conference({ws, myIdName, cameraId, micId, speakerId}) {
   const [me, setMe] = useState(null)
   const [mode, setMode] = useState(false) // 면접모드 true, 일반모드 false
-  const [screenMode, setScreenMode] = useState(false)
+  const [screenShareTry, setScreenShareTry] = useState(false)
   const chatInput = useRef();
   const chatInputBtn = useRef();
   const chatContentBox = useRef();
-  const screenBox = useRef();
   const cameraBtn = useRef();
   const micBtn = useRef();
   const speakerBtn = useRef();
   const screenBtn = useRef();
-  const interviewModeBtn = useRef();
-  const normalModeBtn = useRef();
+  const changeModeBtn = useRef();
   const exitBtn = useRef();
   const router = useRouter();
 
@@ -56,20 +56,11 @@ function Conference({ws, myIdName, cameraId, micId}) {
       video.id = 'video-' + IdName;
       video.autoplay = true;
       video.controls = false;
+      if(video.sinkId !== speakerId) video.setSinkId(speakerId)
       container.appendChild(video);
       container.appendChild(span);
       container.style = "display: flex; flex-direction: column; align-items: center;"
-      // if(this.id) {
-      //   document.getElementById('participants').appendChild(container);
-      // }else {
-      //   // const screen = document.createElement("video")
-      //   // screen.autoplay = true
-      //   // screen.srcObject = stream
-      //   screenBox.current.appendChild(video);
-      //   video.style.height = "100%"
-      //   video.style.width = "100%"
-      //   setScreenMode(true);
-      // }
+
       if(!isCam) {
         // pass
       }else if(this.id === "screen") {
@@ -140,6 +131,12 @@ function Conference({ws, myIdName, cameraId, micId}) {
           onExistingParticipants(jsonMsg);
           break;
         case "newParticipantArrived":
+          if(myIdName === appliedUser) {
+            sendMessage({
+              id: "mode",
+              position: screenShareTryState?`screenShareTry-${appliedUser}`:`notScreenShareTry-${appliedUser}`
+            })
+          }
           onNewParticipant(jsonMsg);
           break;
         case "participantLeft":
@@ -155,6 +152,9 @@ function Conference({ws, myIdName, cameraId, micId}) {
           break;
         case "chat":
           onReceiveChat(jsonMsg);
+          break;
+        case "mode":
+          onMode(jsonMsg);
           break;
         default:
           console.log(`ERROR! ${jsonMsg}`)
@@ -209,57 +209,19 @@ function Conference({ws, myIdName, cameraId, micId}) {
       delete participants[jsonMsg.name];
     }
 
-    const chatContentBoxEl = chatContentBox.current
-    function onReceiveChat(jsonMsg) {
-      const senderIdName = jsonMsg.sessionName
-      const senderName = senderIdName.slice(1 + senderIdName.search('-'), senderIdName.length);
-      const showingMsg = `${senderName}: ${jsonMsg.contents}`
-      const showingMsgEl = document.createElement("h5")
-      showingMsgEl.innerText = showingMsg
-      chatContentBoxEl.appendChild(showingMsgEl)
-    }
-
-    function sendChatMsg() {
-      const chatMsg = chatInputEl.value.trim()
-      if(chatMsg) {
-        sendMessage({
-          id: "chat",
-          contents: chatMsg
-        })
-      }
-      chatInputEl.value = ""
-    }
-    const chatInputEl = chatInput.current
-    const chatInputBtnEl = chatInputBtn.current
-    chatInputBtnEl.addEventListener("click", sendChatMsg)
-
-    const cameraBtnEl = cameraBtn.current
-    const micBtnEl = micBtn.current
-    const speakerBtnEl = speakerBtn.current
-    const screenBtnEl = screenBtn.current
-    const interviewModeBtnEl = interviewModeBtn.current
-    const normalModeBtnEl = normalModeBtn.current
-    const exitBtnEl = exitBtn.current
-    function changeToInterviewMode() {
-      setMode(true)
-    }
-    function changeToNomalMode() {
-      setMode(false)
-    }
-
     function ScreenHandler() {
-      console.log('Loaded ScreenHandler', arguments);
+      // console.log('Loaded ScreenHandler', arguments);
       // REF https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints#Properties_of_shared_screen_tracks
       const constraints = {
         audio: true,
         video: {
-            width: 800, // 최대 너비
-            height: 450, // 최대 높이
-            frameRate: 10, // 최대 프레임
+          width: 800, // 최대 너비
+          height: 450, // 최대 높이
+          frameRate: 10, // 최대 프레임
         },
       };
       let localStream = null;
-  
+
       // 스크린캡쳐 API를 브라우저 호환성 맞게 리턴합니다.
       function getCrossBrowserScreenCapture() {
         if (navigator.getDisplayMedia) {
@@ -292,13 +254,12 @@ function Conference({ws, myIdName, cameraId, micId}) {
 
     function screenMessage(message) {
       const jsonMessage = JSON.stringify(message);
-      console.log("Sending message: " + jsonMessage);
+      // console.log("Sending message: " + jsonMessage);
       screenWs.send(jsonMessage);
     }
     let screen;
     function existingScreens(jsonMsg) {
       screen = new Participant(`screen-${myIdName}`, false);
-      console.log(stream)
       const options = {
         videoStream: stream,
         mediaConstraints: {
@@ -318,50 +279,108 @@ function Conference({ws, myIdName, cameraId, micId}) {
       });
     }
 
+    let appliedUser = "";
+    function onMode(jsonMsg) {
+      const idx = jsonMsg.position.search('-')
+      const position = jsonMsg.position.slice(0, idx)
+      appliedUser = jsonMsg.position.slice(1 + idx, jsonMsg.position.length);
+      console.log(position, appliedUser)
+      if(appliedUser !== myIdName) {
+        switch(position) {
+          case "screenShareTry":
+            screenShareTryState = true
+            break
+          case "notScreenShareTry":
+            screenShareTryState = false
+            break
+          case "screenShare":
+            screenShareState = true
+            screenShareTryState = true
+            break
+          case "notScreenShare":
+            screenShareState = false
+            screenShareTryState = false
+            break
+          default:
+            console.log(`ERROR! ${jsonMsg.mode}`)
+        }
+        setScreenShareTry(screenShareTryState);
+      }
+    }
+
     let stream;
     let screenWs;
+    let screenShareState = false
+    let screenShareTryState = false
     async function startScreenShare() {
       if(!stream) {
         screenWs = new SockJS(`${WEBRTC_URL}/groupcall`);
+        screenShareTryState = true
+        sendMessage({
+          id: "mode",
+          position: `screenShareTry-${myIdName}`
+        })
+        appliedUser = myIdName
         const screenHandler = new ScreenHandler();
         stream = await screenHandler.start();
         if(stream) {
-          screenMessage({
-            id: "joinRoom",
-            name: `screen-${myIdName}`,
-            room: roomName,
-          });
-          screenWs.onmessage = function(message) {
-            const jsonMsg = JSON.parse(message.data);
-            console.log("Received message: " + message.data);
-            switch(jsonMsg.id) {
-              case "existingParticipants":
-                existingScreens(jsonMsg)
-                break;
-              case "newParticipantArrived":
-                console.log(`111111 ${jsonMsg}`)
-                break;
-              case "participantLeft":
-                console.log(`222222 ${jsonMsg}`)
-                break;
-              case "receiveVideoAnswer":
-                screen.rtcPeer.processAnswer(jsonMsg.sdpAnswer, function(error) {
-                  if(error) return console.log(`ERROR! ${error}`);
-                });
-                break;
-              case "iceCandidate":
-                screen.rtcPeer.addIceCandidate(jsonMsg.candidate, function (error) {
-                  if (error) return console.log(`ERROR! ${error}`);
-                });
-                break;
-              default:
-                console.log(`ERROR! ${jsonMsg}`)
+          if(!screenShareState) {
+            // screenShareTryState = false
+            setScreenShareTry(false);
+            screenShareState = true
+            sendMessage({
+              id: "mode",
+              position: `screenShare-${myIdName}`
+            })
+            screenMessage({
+              id: "joinRoom",
+              name: `screen-${myIdName}`,
+              room: roomName,
+            });
+            screenWs.onmessage = function(message) {
+              const jsonMsg = JSON.parse(message.data);
+              // console.log("Received message: " + message.data);
+              switch(jsonMsg.id) {
+                case "existingParticipants":
+                  existingScreens(jsonMsg)
+                  break;
+                case "newParticipantArrived":
+                  break;
+                case "participantLeft":
+                  break;
+                case "receiveVideoAnswer":
+                  screen.rtcPeer.processAnswer(jsonMsg.sdpAnswer, function(error) {
+                    if(error) return console.log(`ERROR! ${error}`);
+                  });
+                  break;
+                case "iceCandidate":
+                  screen.rtcPeer.addIceCandidate(jsonMsg.candidate, function (error) {
+                    if (error) return console.log(`ERROR! ${error}`);
+                  });
+                  break;
+                default:
+                  console.log(`ERROR! ${jsonMsg}`)
+              }
             }
+          }else {
+            alert("화면은 한 명만 공유 가능합니다.")
           }
         }else {
+          screenShareTryState = false
+          sendMessage({
+            id: "mode",
+            position: `notScreenShareTry-${myIdName}`
+          })
           screenWs.close();
         }
       }else {
+        screenShareTryState = false
+        setScreenShareTry(screenShareTryState);
+        screenShareState = false
+        sendMessage({
+          id: "mode",
+          position: `notScreenShare-${myIdName}`
+        })
         stream = null;
         screenMessage({
           id: "leaveRoom"
@@ -370,9 +389,48 @@ function Conference({ws, myIdName, cameraId, micId}) {
       }
     }
 
+    const chatContentBoxEl = chatContentBox.current
+    function onReceiveChat(jsonMsg) {
+      const senderIdName = jsonMsg.sessionName
+      console.log(senderIdName)
+      const senderName = senderIdName.slice(1 + senderIdName.search('-'), senderIdName.length);
+      const showingMsg = `${senderName}: ${jsonMsg.contents}`
+      const showingMsgEl = document.createElement("h5")
+      showingMsgEl.innerText = showingMsg
+      chatContentBoxEl.appendChild(showingMsgEl)
+    }
 
-    normalModeBtnEl.addEventListener("click", changeToNomalMode)
-    interviewModeBtnEl.addEventListener("click", changeToInterviewMode)
+    function sendChatMsg() {
+      const chatMsg = chatInputEl.value.trim()
+      if(chatMsg) {
+        sendMessage({
+          id: "chat",
+          contents: chatMsg
+        })
+      }
+      chatInputEl.value = ""
+    }
+    const chatInputEl = chatInput.current
+    const chatInputBtnEl = chatInputBtn.current
+    chatInputBtnEl.addEventListener("click", sendChatMsg)
+
+    const cameraBtnEl = cameraBtn.current
+    const micBtnEl = micBtn.current
+    const speakerBtnEl = speakerBtn.current
+    const screenBtnEl = screenBtn.current
+    const changeModeBtnEl = changeModeBtn.current
+    const exitBtnEl = exitBtn.current
+
+    let modeState = false;
+    function changeMode() {
+      modeState = !modeState
+      setMode(modeState)
+      sendMessage({
+        id: "mode",
+        position: modeState?"volunteer":"normal"
+      })
+    }
+    changeModeBtnEl.addEventListener("click", changeMode)
     screenBtnEl.addEventListener("click", startScreenShare)
     // 방 입장
     sendMessage({
@@ -381,12 +439,38 @@ function Conference({ws, myIdName, cameraId, micId}) {
       room: roomName,
     });
 
+    window.addEventListener("beforeunload", () => {
+      if(stream) {
+        sendMessage({
+          id: "mode",
+          position: `notScreenShare-${myIdName}`
+        })
+        screenMessage({
+          id: "leaveRoom"
+        });
+        screenWs.close();
+      }
+      sendMessage({
+        id: "leaveRoom"
+      });
+      for (let key in participants) {
+        participants[key].dispose();
+      }
+    })
     return () => {
       chatInputBtnEl.removeEventListener("click", sendChatMsg)
-      normalModeBtnEl.removeEventListener("click", changeToNomalMode)
-      interviewModeBtnEl.removeEventListener("click", changeToInterviewMode)
       screenBtnEl.removeEventListener("click", startScreenShare)
       // 방 퇴장
+      if(stream) {
+        sendMessage({
+          id: "mode",
+          position: `notScreenShare-${myIdName}`
+        })
+        screenMessage({
+          id: "leaveRoom"
+        });
+        screenWs.close();
+      }
       sendMessage({
         id: "leaveRoom"
       });
@@ -409,22 +493,34 @@ function Conference({ws, myIdName, cameraId, micId}) {
     }
   }, [cameraId, micId])
 
+  useEffect(() => {
+    for(let container of document.querySelector("#participants").children) {
+      const video = container.firstChild
+      if(video.sinkId !== speakerId) video.setSinkId(speakerId)
+    }
+    for(let container of document.querySelector("#participants").children) {
+      const video = container.firstChild
+      if(video.sinkId !== speakerId) video.setSinkId(speakerId)
+    }
+  },[speakerId])
   return <div className={styles.mainContainer}>
     <div className={styles.videoContainer}>
       <div id="screens" ></div>
-      <div ref={screenBox} style={!screenMode?{display: "none"}:{width: "60vw", height:"33.75vw"}}></div>
       <div className={styles.faces} id="participants"></div>
       <div className={styles.btnBar}>
         <span ref={cameraBtn}><i className="fas fa-video"></i></span>
         <span ref={micBtn}><i className="fas fa-microphone"></i></span>
         <span ref={speakerBtn}><i className="fas fa-volume-up"></i></span>
-        <span ref={screenBtn}><i className="fas fa-tv"></i></span>
-        <span ref={interviewModeBtn} style={mode?{display: "none"}:{}}><i className="fas fa-user-tie"></i></span>
-        <span ref={normalModeBtn} style={!mode?{display: "none"}:{}}><i className="fas fa-users"></i></span>
+        <button disabled={screenShareTry} ref={screenBtn}><i className="fas fa-tv"></i></button>
+        <span ref={changeModeBtn} >
+          <span style={mode?{display: "none"}:{}}><i className="fas fa-user-tie"></i></span>
+          <span style={!mode?{display: "none"}:{}}><i className="fas fa-users"></i></span>
+        </span>
         <span ref={exitBtn}><i className="fas fa-times-circle"></i></span>
       </div>
       <CameraSelect />
       <MicSelect />
+      <SpeakerSelect />
     </div>
     <div className={styles.chat} id="chat">
       <h3>채팅창</h3>
