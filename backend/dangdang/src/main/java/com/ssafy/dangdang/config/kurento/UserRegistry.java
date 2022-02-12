35fe612da6b3a8a9ac28e09993b092f5f11e8d33
LimHyuk <skdltm117@gmail.com>
2022-02-13 00:37:28 +0900

    [S06P12C203-341] fix : 화면 공유 되어있는 방에서 퇴장시 발생하던 버그 픽스

--- backend/dangdang/src/main/java/com/ssafy/dangdang/config/kurento/UserRegistry.java
+++ backend/dangdang/src/main/java/com/ssafy/dangdang/config/kurento/UserRegistry.java
@@ -25,7 +25,7 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Map of users registered in the system. This class has a concurrent hash map to store users, using
  * its name as key in the map.
- * 
+ *
  * @author Boni Garcia (bgarcia@gsyc.es)
  * @author Micael Gallego (micael.gallego@gmail.com)
  * @since 5.0.0
@@ -33,42 +33,48 @@ import java.util.concurrent.ConcurrentHashMap;
 @Getter
 @Setter
 public class UserRegistry {
-
-  private final ConcurrentHashMap<String, UserSession> usersByName = new ConcurrentHashMap<>();
-  private ConcurrentHashMap<String, UserSession> usersBySessionId = new ConcurrentHashMap<>();
+    
+    private final ConcurrentHashMap<String, UserSession> usersByName = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, UserSession> usersBySessionId = new ConcurrentHashMap<>();
 
 //  public void register(UserSession user) {
 //    usersBySessionId.put(user.getId(), user);
 //  }
-
-  public void register(UserSession user) {
-    usersByName.put(user.getName(), user);
-    usersBySessionId.put(user.getSession().getId(), user);
-  }
-
-  public UserSession getById(String id) {
-    return usersBySessionId.get(id);
-  }
-
-  public UserSession getBySession(WebSocketSession session) {
-    return usersBySessionId.get(session.getId());
-  }
-  public UserSession getByName(String name) {
-    return usersByName.get(name);
-  }
-
-  // TODO: id와 name 모두 담는 객체 만들기
+    
+    public void register(UserSession user) {
+        usersByName.put(user.getName(), user);
+        usersBySessionId.put(user.getSession().getId(), user);
+    }
+    
+    public UserSession getById(String id) {
+        return usersBySessionId.get(id);
+    }
+    
+    public UserSession getBySession(WebSocketSession session) {
+        return usersBySessionId.get(session.getId());
+    }
+    
+    public UserSession getByName(String name) {
+        return usersByName.get(name);
+    }
+    
+    // TODO: id와 name 모두 담는 객체 만들기
 //  public boolean exists(String id) {
 //    return usersBySessionId.keySet().contains(id);
 //  }
     public boolean exists(String name) {
-    return usersByName.keySet().contains(name);
-  }
-  public UserSession removeBySession(WebSocketSession session) {
-    final UserSession user = getBySession(session);
-    usersByName.remove(user.getName());
-    usersBySessionId.remove(session.getId());
-    return user;
-  }
+        return usersByName.keySet().contains(name);
+    }
+    
+    public UserSession removeBySession(WebSocketSession session) {
+        final UserSession user = getBySession(session);
 
+        if(user == null) return null;
+        if (usersByName.containsKey(user.getName()))
+            usersByName.remove(user.getName());
+        if (usersBySessionId.containsKey(session.getId()))
+            usersBySessionId.remove(session.getId());
+        return user;
+    }
+    
 }
