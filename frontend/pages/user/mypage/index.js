3925f6dfc02590e983f239287aeb311cae0ebe2b
김동유 <ehddb2252@gmail.com>
2022-02-14 11:19:44 +0900

    fix: user 수정

--- frontend/pages/user/mypage/index.js
+++ frontend/pages/user/mypage/index.js
@@ -7,16 +7,18 @@ import { useRouter } from "next/router";
 import { connect } from "react-redux";
 
 function mapStateToProps({ userReducer }) {
+  const isImgUrlBackendServer = userReducer.user.imageUrl.slice(0, 5) !== "https"
   return {
     user: userReducer.user,
+    isImgUrlBackendServer,
   };
 }
 
 export default connect(mapStateToProps)(userInfo);
 
-function userInfo({ user }) {
+function userInfo({ user, isImgUrlBackendServer }) {
   const router = useRouter();
-
+  const
   //회원 탈퇴
   const onLeaveDangDang = () => {
     leaveDangDang(
@@ -35,7 +37,11 @@ function userInfo({ user }) {
   return (
     <div className={styles.myPageContainer}>
       <div className={styles.imageBox}>
-        <img src={`${BACKEND_URL}/files/images/${user.imageUrl}`} />
+        {
+          isImgUrlBackendServer?
+          <img src={`${BACKEND_URL}/files/images/${user.imageUrl}`} />:
+          <img src={`${user.imageUrl}`} />
+        }
       </div>
 
       <div className={styles.info}>
