fa0223ba9eb1c3134eb83b6dc41f9c0d958b6116
김혜인 <fallingstars2749@gmail.com>
2022-02-15 03:41:18 +0900

    [S06P12C203-361] feat: 관리자 페이지 질문관리 UI

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,20 +1,31 @@
 import { useState } from "react";
 import AdminQuestions from "../../components/admin/AdminQuestions";
 import AdminUsers from "../../components/admin/AdminUsers";
+import styles from "../../scss/admin/main.module.scss";
 
 export default function Admin() {
   const [questionMode, setQuestionMode] = useState(true);
 
   return (
-    <div>
-      <h1>관리자 페이지</h1>
-
-      <nav>
-        <li onClick={() => setQuestionMode(true)}>질문</li>
-        <li onClick={() => setQuestionMode(false)}>유저</li>
+    <div className={styles.mainContainer}>
+      <nav className={styles.menu}>
+        <li
+          onClick={() => setQuestionMode(true)}
+          className={questionMode ? styles.questionMode : null}
+        >
+          질문 관리
+        </li>
+        <li
+          onClick={() => setQuestionMode(false)}
+          className={questionMode ? null : styles.userMode}
+        >
+          회원 관리
+        </li>
       </nav>
 
-      {questionMode ? <AdminQuestions /> : <AdminUsers />}
+      <div className={styles.container}>
+        {questionMode ? <AdminQuestions /> : <AdminUsers />}
+      </div>
     </div>
   );
 }
